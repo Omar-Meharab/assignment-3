dd6229f2d74d1135742296aaba3ba435bae31668
Omar Meharab <omarmeharab81@gmail.com>
2021-01-22 17:13:12 +0600

    kilometerToMeter done

--- assignment.js
+++ assignment.js
@@ -1,7 +1,20 @@
 // https://github.com/Omar-Meharab/assignment-3
-console.log("hello world");
 
 // kilometerToMeter
+
+function kilometerToMeter(distance) {
+    if (distance > 0) {
+        convertedDistance = distance * 1000;
+        return convertedDistance;
+    }
+    else if(distance <= 0) {
+        console.log("Distance can not be negative/zero.");
+    }
+    else {
+        console.log("Enter a value to convert to meter.");
+    }
+}
+
 
 // budgetCalculator
 
